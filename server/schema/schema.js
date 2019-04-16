a5a9636930d9eb3b7c1cc9402655963d3e9fb1eb
Peter Ramos <peterdramos02@gmail.com>
2019-04-16 17:39:25 -0400

    fix some typos and remove duplicates in book data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,8 +13,8 @@ const {
 // dummy data
 const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2',  authorId: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3',  authorId: '3' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
   { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
   { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
@@ -49,7 +49,7 @@ const AuthorType = new GraphQLObjectType({
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
     books: {
-      author: new GraphQLList(BookType),
+      type: new GraphQLList(BookType),
       resolve(parent, args) {
         return _.filter(books, { authorId: parent.id});
       }
