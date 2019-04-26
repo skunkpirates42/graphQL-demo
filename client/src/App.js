fdaaa3540e22ad0e4ebf143093166eb6eff0f866
Peter Ramos <peterdramos02@gmail.com>
2019-04-26 00:37:47 -0400

    apollo stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo';
 import BookList from './components/BookList';
 
 
 // apollo client set-up
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
-})
+});
+
 class App extends Component {
   render() {
     return (
