2e854031b5baf476e93171c285078d106b37c8f9
Peter Ramos <peterdramos02@gmail.com>
2019-04-25 23:55:33 -0400

    change resolve functions to query mongodb

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -23,7 +23,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         console.log(parent);
-        // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -38,7 +38,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return _.filter(books, { authorId: parent.id});
+        return Book.find({ authorId: parent.id });
       }
     }
   })
@@ -52,7 +52,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/ other source
-        // return _.find(books, {id: args.id});
+        return Book.findById(args.id);
       }
     },
     author: {
@@ -60,18 +60,20 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return books;
+        return Book.find({});
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        // return authors;
+        return Author.find({});
+
       }
     }
   }
