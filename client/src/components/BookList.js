fd8ef8f47aa20d9251a3a3c4a1e5ed4a339e6a23
Peter Ramos <peterdramos02@gmail.com>
2019-04-26 18:35:41 -0400

    render data from query to view in BookList component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
@@ -11,15 +11,32 @@ const getBooksQuery = gql`
   }
 `
 
-function BookList(props) {
-  console.log(props)
-  return (
-    <div>
-      <ul id="book-list">
-        <li>Book name</li>
-      </ul>
-    </div>
-  );
+class BookList extends Component {
+  
+  displayBooks() {
+    const { data } = this.props;
+    if (data.loading) {
+      return (
+        <div>Loading Books...</div>
+      )
+    } else {
+        return data.books.map(book => {
+          return <li key={book.id}>{book.name}</li>
+        }
+        )
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          { this.displayBooks() }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default graphql(getBooksQuery)(BookList);
