dd7bc98cb443e7f51b5e915db63e680892337084
Peter Ramos <peterdramos02@gmail.com>
2019-04-26 18:29:03 -0400

    build graphql query in booklist component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-function BookList() {
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
+
+function BookList(props) {
+  console.log(props)
   return (
     <div>
       <ul id="book-list">
@@ -10,4 +22,4 @@ function BookList() {
   );
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
