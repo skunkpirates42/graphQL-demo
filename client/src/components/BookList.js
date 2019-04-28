02f8988be1217256eaf9b62a6ac53fdaf23c4714
Peter Ramos <peterdramos02@gmail.com>
2019-04-28 18:40:57 -0400

    add logic to BookDetails query and pass book id prop in as variable to graph ql query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BookDetails from './BookDetails';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
 class BookList extends Component {
-  
+  state = {
+    selected: null
+  }
+
   displayBooks() {
     const { data } = this.props;
     if (data.loading) {
@@ -12,7 +16,7 @@ class BookList extends Component {
       )
     } else {
         return data.books.map(book => {
-          return <li key={book.id}>{book.name}</li>
+          return <li onClick={e => this.setState({ selected: book.id })} key={book.id}>{book.name}</li>
         }
         )
 
@@ -25,6 +29,7 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
+        <BookDetails bookId={ this.state.selected } />
       </div>
     );
   }
