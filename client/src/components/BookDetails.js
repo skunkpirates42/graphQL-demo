a6abccbd9c7c66fe2fd329f4238757d6773e04c1
Peter Ramos <peterdramos02@gmail.com>
2019-04-27 20:13:07 -0400

    add BookDetails component

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BookDetails extends Component {
+  render() {
+    return (
+      <div id="book-details">
+        <p>Output book details here</p>
+      </div>
+    )
+  }
+}
